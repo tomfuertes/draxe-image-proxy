2b2ad3b1fd824851a0e79d775082faa2a7a183de
Tom Fuertes <tomfuertes@gmail.com>
2016-02-26 12:30:14 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 var express = require('express');
 var app = express();
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.route('/*$').all(function(req, res) { // proxy all requests
+  proxy.web(req, res, {
+    target: 'http://cdn.shopify.com/',
+    changeOrigin: true
+  }); //sandbox
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
